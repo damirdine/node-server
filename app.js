a20864d98ed371644fd8573e9e2e6cac373bdf87
damirdine <damirdine@outlook.fr>
2022-06-16 16:25:42 +0200

    test

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/movies');
+var collection = db.get('movies');
+collection.find({},{},function(e,docs){
+  //console.log(docs);
+});
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -32,7 +40,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
