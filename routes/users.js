974ef68228dcba0ecfd0672410651bd38faab9c9
damirdine <damirdine@outlook.fr>
2022-06-17 10:54:43 +0200

    add User to db

--- routes/users.js
+++ routes/users.js
@@ -18,4 +18,40 @@ router.get('/userlist',function(req,res){
   });
 });
 
+router.post('/adduser',function(req,res){
+  var db = req.db;
+  
+  //post value from dthe form 
+
+  var userName = req.body.username;
+  var userEmail = req.body.useremail;
+  var userFullName = req.body.userfullname;
+  var userAge = req.body.userage;
+  var userLocation = req.body.userlocation;
+  var userGender = req.body.usergender;
+
+  //Set the collection
+  var collection = db.get('userlist');
+
+  collection.insert(
+    {
+      "username" : userName,
+      "useremail": userEmail,
+      "userfullname":userFullName,
+      "userage":userAge,
+      "userlocation":userLocation,
+      "usergender":userGender
+    }, 
+    function(err,doc){
+      if(err){
+        res.send("Problem for adding user to database.")
+      }
+      res.redirect("userlist");
+    }
+    
+
+  )
+
+}
+)
 module.exports = router;
